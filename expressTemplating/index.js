864a22a761c15438adf2afc89130d2886a667061
xxxxxxh <h47671590@gmail.com>
2020-11-14 14:50:41 +0800

    11/14

---
+++ expressTemplating/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+app.get('/', (req, res) => {
+	res.render('home')
+})
+
+app.get('/r/:subreddit', (req, res) => {
+	const { subreddit } = req.params;
+	res.render('subreddit', {subreddit})
+})
+
+app.get('/rand',  (req, res) => {
+	const num = Math.floor(Math.random()*10) + 1;
+	res.render('random', {rand: num});  // or just num in{}
+} )
+
+app.listen(3000, () => {
+	console.log('listening on port 3000')
+})
