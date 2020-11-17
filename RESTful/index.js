3079a6120c639e202c2b7bfdf24d5fa02208e851
xxxxxxh <h47671590@gmail.com>
2020-11-17 22:03:36 +0800

    11/17

--- RESTful/index.js
+++ RESTful/index.js
@@ -1,7 +1,40 @@
 const express = require('express')
 const app = express();
+const path = require("path");
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
+const comments = [
+	{
+		username: 'Todd',
+		comment: 'lol that is so funny!'
+	},
+	{
+		username: 'Skyler',
+		comment: "cycyc that is so funny!"
+	},
+	{
+		username: 'Boil',
+		comment: 'pleassszzz delete your account, Todd'
+	}
+]
+app.get('/comments', (req, res) => {
+	res.render('comments/index', { comments })
+})
+
+app.get('/comments/new', (req, res) => {
+	res.render('comments/new')
+})
+
+app.post('/comments',(req, res) => {
+	// console.log(req.body)
+	const { username, comment } = req.body;
+	comments.push({ username, comment})
+	res.send("it worked!")
+})
 
 app.get('/tacos', (req, res) => {
 	res.send('get /tacos response')
