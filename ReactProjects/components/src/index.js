7d35d8f12acc852e2a8623c8d9ae3c277ff52555
Xuh038 <xuh038@ucsd.edu>
2020-12-28 17:46:11 +0800

    1228

--- ReactProjects/components/src/index.js
+++ ReactProjects/components/src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import faker from "faker";
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-		
+			<CommentDetail
+				author="Sam"
+				date="Today at 6:00PM"
+				text="Nice Blog Post"
+				img={faker.image.image()}
+			/>
+			<CommentDetail
+				author="Jane"
+				date="Today at 2:00AM"
+				text="222"
+				img={faker.image.image()}
+			/>
+			<CommentDetail
+				author="Joy"
+				date="Today at 1:00PM"
+				text="233"
+				img={faker.image.image()}
+			/>
 		</div>
 	);
-}
+};
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
