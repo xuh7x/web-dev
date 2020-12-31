fd6b5b1de11c33400e2fe32ccd9777f8487c7ef1
Xuh038 <xuh038@ucsd.edu>
2020-12-31 14:21:56 +0800

    1231

--- ReactProjects/components/src/ApprovalCard.js
+++ ReactProjects/components/src/ApprovalCard.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import CommentDetail from "./CommentDetail";
 
 const ApprovalCard = (props) => {
+	// console.log(props.children);
 	return (
 		<div className="ui card">
-			<div className="content">
-				are u sure?
-			</div>
+			<div className="content">{props.children}</div>
 			<div className="extra content">
 				<div className="ui two buttons">
-					<div className="ui basic green button">
-						Approve
-					</div>
-					<div className="ui basic red button">
-						Reject
-					</div>
+					<div className="ui basic green button">Approve</div>
+					<div className="ui basic red button">Reject</div>
 				</div>
 			</div>
 		</div>
--- ReactProjects/components/src/index.js
+++ ReactProjects/components/src/index.js
@@ -2,28 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail
-				author="Sam"
-				date="Today at 6:00PM"
-				text="Nice Blog Post"
-				img={faker.image.image()}
-			/>
-			<CommentDetail
-				author="Jane"
-				date="Today at 2:00AM"
-				text="222"
-				img={faker.image.image()}
-			/>
-			<CommentDetail
-				author="Joy"
-				date="Today at 1:00PM"
-				text="233"
-				img={faker.image.image()}
-			/>
+			<ApprovalCard>
+				<CommentDetail
+					author="Sam"
+					date="Today at 6:00PM"
+					text="Nice Blog Post"
+					img={faker.image.image()}
+				/>
+			</ApprovalCard>
+			
+			<ApprovalCard>
+				<CommentDetail
+					author="Jane"
+					date="Today at 2:00AM"
+					text="222"
+					img={faker.image.image()}
+				/>
+			</ApprovalCard>
+			
+			<ApprovalCard>
+				<CommentDetail
+					author="Joy"
+					date="Today at 1:00PM"
+					text="233"
+					img={faker.image.image()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };
