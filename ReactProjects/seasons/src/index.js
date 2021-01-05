3498c9a2f430f892c7fcbb393255deec8d2ebf73
Xuh038 <xuh038@ucsd.edu>
2021-01-05 17:52:50 +0800

    1231

--- ReactProjects/seasons/src/index.js
+++ ReactProjects/seasons/src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-	return <div>hi there!</div>
-}
+// const App = () => {
+// 	window.navigator.geolocation.getCurrentPosition(
+// 		(position) => {
+// 			console.log(position);
+// 		},
+// 		(err) => console.log(err)
+// 	);
+// 	return <div>latitude: </div>
+// }
 
+class App extends React.Component {
+	// constructor func is the very first func that is gonna be called when this class is created
+	constructor(porps) {
+		// 1 way to initialize state
+		super(porps);   // super - parent's constructor function
+		
+		this.state = { lat: null, }  // if the value is #, set the default to 'null'
+	}
+	
+	// render method - is required for every react component we created - have to define render
+	render() {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				console.log(position);
+			},
+			(err) => console.log(err)
+		);
+		return <div>Latitude: </div>
+	}
+}
 
 ReactDOM.render(
 	<App/>,
