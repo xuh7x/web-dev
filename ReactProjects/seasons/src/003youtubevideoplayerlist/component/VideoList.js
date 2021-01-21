7dd142358cdf5aacd6791ae2af99758d3f7dc475
Xuh038 <xuh038@ucsd.edu>
2021-01-21 17:22:57 +0800

    0121 video player list done

--- ReactProjects/seasons/src/003youtubevideoplayerlist/component/VideoList.js
+++ ReactProjects/seasons/src/003youtubevideoplayerlist/component/VideoList.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import VideoItem from "./VideoItem";
 
-const VideoList = ({videos}) =>  {
+const VideoList = ({videos, onVideoSelect}) =>  {  // destructure
+	console.log(videos)
 	const renderedList = videos.map(video => {
-		return <VideoItem video={video}/>
+		return <VideoItem
+			key={video.id.videoId}
+			video={video}
+			onVideoSelect={onVideoSelect}
+		/>
 	})
 		return (
 			<div className="ui relaxed divided list">
