39c197ec3ba98384a5f8c803b57d0be0b9016305
Xuh038 <xuh038@ucsd.edu>
2021-02-19 17:43:33 +0800

    210219 useEffect basic

--- ReactProjects/seasons/src/components/Search.js
+++ ReactProjects/seasons/src/components/Search.js
@@ -1,7 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
 	const [term, setTerm] = useState('');
+	
+	useEffect(() => {
+	
+	}, [term])
+	
 	return (
 		<div className="ui form">
 			<div className="field">
