9f2b720ecdc3ace265134051de4784d5964a85f8
Xuh038 <xuh038@ucsd.edu>
2021-02-20 17:57:37 +0800

    210220 useEffect basic

--- ReactProjects/seasons/src/components/Search.js
+++ ReactProjects/seasons/src/components/Search.js
@@ -3,21 +3,74 @@ import axios from 'axios';
 
 const Search = () => {
 	const [term, setTerm] = useState('');
+	const [results, setResults] = useState([]);
+	console.log(results)
+	useEffect( () => {
+		// make request anytime term changes
+		// we cannot make effect func as async func. instead, write the async func inside of effect and call it immediately
+		// const search = async () => {   // method 1
+		// 	const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+		// 		params: {  // axios will code params as query string, and append on to the end of the url above
+		// 			action: 'query',
+		// 			list: 'search',
+		// 			origin: '*',
+		// 			format: 'json',
+		// 			srsearch: term,
+		// 		}
+		// 	});
+		// 	setResults(data.query.search);
+		// }
+		// const timeoutId = setTimeout(() => {
+		// 	if (term) search();
+		// }, 500)
+			// if (term) search();  // or make a default search term
+		// method 2            same as #1  no performance benefit at all, use either readable
+		// (async () => {
+		// 	await axios.get('222')
+		// })();
+		// method 3
+		// axios.get('111')
+		// 	.then((response) => {
+		// 		console.log(response.data)
+		// 	});
+		
+	}, [term]);
 	
-	useEffect(() => {
-	
-	}, [term])
+	const renderedResults = results.map((res) => {
+		return (
+			<div className="item" key={res.pageid}>
+				<div className="right floated content">
+					<a 
+						className="ui button"
+						href={`https://en.wikipedia.org?curid=${res.pageid}`}
+					>Go
+					</a>
+				</div>
+				<div className="content">
+					<div className="header">{res.title}</div>
+					{/*anytime u take a string from 3rd part like this, u could be introducing a security hole into ur application
+					 - XSS attack - kind = cross site scripting attack - that is where we accidentally pickup and render some HTML
+					  from an untrusted source that can allow hacker to execute some JS inside of our application.*/}
+					<span dangerouslySetInnerHTML={{ __html: res.snippet }}></span>
+					{/*{res.snippet}*/}
+				</div>
+			</div>
+		)
+	})
 	
 	return (
-		<div className="ui form">
-			<div className="field">
-				<label htmlFor="input">Enter Search Term: </label>
-				<input id="input"
-				       className="input"
-				       value={term}
-				       onChange={e => setTerm(e.target.value)}
-				/>
+		<div>
+			<div className="ui form">
+				<div className="field">
+					<label htmlFor="input">Enter Search Term: </label>
+					<input id="input"
+					       className="input"
+					       value={term}
+					       onChange={e => setTerm(e.target.value)}
+					/>
+				</div>
 			</div>
+			<div className="ui celled list">{renderedResults}</div>
 		</div>
 	)
 }
